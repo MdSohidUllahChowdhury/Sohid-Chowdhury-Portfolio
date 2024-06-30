a4396f30728ef77d139de97f572b98298643fb1d
Sohid Ullah <mdsohidullahchowdhury1@gmail.com>
2024-06-30 14:01:43 +0600

    Portfolio

---
+++ android/app/src/main/kotlin/com/example/sohid_chowdhury_portfolio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sohid_chowdhury_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Model/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sohid_chowdhury_portfolio/View/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData( 
+        useMaterial3: true,
+      ),
+      home:const HomeScreen() ,
+      );  
+  }
+}
